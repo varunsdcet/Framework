c440e793a74c8aa104b72274afcfe178c6306bda
APPLEMACBOOK PRO A1502 <mbpa1502@APPLEMACBOOKs-MacBook-Pro.local>
2020-09-27 13:23:20 +0530

    init

---
+++ varunframework/File.swift
@@ -0,0 +1,16 @@
+//
+//  File.swift
+//  varunframework
+//
+//  Created by APPLEMACBOOK PRO A1502 on 27/09/20.
+//  Copyright © 2020 APPLEMACBOOK PRO A1502. All rights reserved.
+//
+
+import Foundation
+public class File {
+    public init() {}
+    open func log(message : String){
+        print(message)
+    }
+    
+}
---
+++ varunframework/varunframework.h
@@ -0,0 +1,19 @@
+//
+//  varunframework.h
+//  varunframework
+//
+//  Created by APPLEMACBOOK PRO A1502 on 27/09/20.
+//  Copyright © 2020 APPLEMACBOOK PRO A1502. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for varunframework.
+FOUNDATION_EXPORT double varunframeworkVersionNumber;
+
+//! Project version string for varunframework.
+FOUNDATION_EXPORT const unsigned char varunframeworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <varunframework/PublicHeader.h>
+
+
